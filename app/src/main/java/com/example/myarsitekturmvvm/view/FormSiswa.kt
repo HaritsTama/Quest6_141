c6cf412c90c9e1afcc36f8c481bd4929716c535a
HaritsTama <nayotama1818@gmail.com>
2025-11-20 15:55:19 +0700

    Menambahkan 4 variable

--- app/src/main/java/com/example/myarsitekturmvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/FormSiswa.kt
@@ -1,6 +1,9 @@
 package com.example.myarsitekturmvvm.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import org.jetbrains.annotations.ApiStatus
 import java.lang.reflect.Modifier
 
@@ -9,4 +12,10 @@ fun FormSiswa(
     pilihanJK : List<String>,
     onSubmitButtonClicked : (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
-){}
+){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember{ mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    var listData by MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+
+}
